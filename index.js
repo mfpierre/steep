ca80ccc428e5fec438dfec46ca23f1a42a29eaf1
Pierre <mfpierre@gmail.com>
2015-05-31 18:58:46 +0200

    Add tea type duration

--- index.js
+++ index.js
@@ -4,11 +4,36 @@
  * Module dependencies.
  */
 
-var program = require('commander');
+var program  = require('commander');
+var teaTimes = {
+  'black': 180,
+  'green': 120,
+  'herbal': 240,
+  'oolong': 180,
+  'white': 420,
+  'mate': 300,
+  'rooibos': 300,
+  'chai': 600,
+  'darjeeling': 180
+};
 
 program
   .version('0.0.1')
   .option('-t, --tea [type]', 'Steep the specified type of tea [type]', 'black')
+  .option('-s, --strong', '20% additional steep time')
+  .option('-l, --light', '20% less steep time')
   .parse(process.argv);
 
-console.log('Brewing a %s tea', program.tea);
+function getTimeForType(type)
+{
+  for(var index in teaTimes) {
+    if(type === index) return teaTimes[index];
+  }
+
+  return 180;
+}
+
+var duration = getTimeForType(program.tea);
+if(program.strong) duration *= 1.2;
+if(program.light) duration *= 0.8;
+console.log('Brewing a %s tea for %s seconds', program.tea, duration);
